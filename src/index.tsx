ff400059aa9f7a4ed758456b400113e72c4d858b
Maksim Dygai <maksimdygai@gmail.com>
2021-09-20 21:33:45 +0300

    Set up application theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { App } from "./App";
-import "./index.css";
 import { store } from "./store/store";
+import { darkTheme } from "./theme/theme";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Helvetica,
+      Arial,
+      sans-serif,
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol";
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <>
+          <App />
+          <GlobalStyle />
+        </>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
