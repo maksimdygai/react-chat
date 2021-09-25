1f027d6dffa876bfb9a7278f86efb8a76613ffb5
Maksim Dygai <maksimdygai@gmail.com>
2021-09-25 21:39:24 +0300

    Clean tests setup file

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
-import {configure, mount, render, shallow} from 'enzyme';
+import {configure} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import React from 'react';
 
@@ -7,7 +7,3 @@ configure({adapter: new Adapter()});
 
 // Define globals to cut down on imports in test files
 global.React = React;
-// @ts-ignore
-global.render = render;
-// @ts-ignore
-global.mount = mount;
